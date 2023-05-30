1b63dd6c03246722a7f4737f5d1fe2ab211cd362
OLUMCUL <mustafaerdemuzum@gmail.com>
2023-05-31 01:54:56 +0300

    31/05/23

--- src/test/java/techproed/Test/Amazon_Select_Test.java
+++ src/test/java/techproed/Test/Amazon_Select_Test.java
@@ -1,7 +1,14 @@
 package techproed.Test;
 
+import Utils.Utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Amazon_Select_Test {
     public static void main(String[] args) {
@@ -10,8 +17,56 @@ public class Amazon_Select_Test {
         driver.manage().window().maximize();
 
         //Amazon sayfasına gidiniz
+        driver.get("https://www.amazon.com.tr/");
         //iphone aratınız
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+
+        WebElement cerez = driver.findElement(By.id("sp-cc-accept"));
+        cerez.click();
+
+        //Utils.findElById("twotabsearchtextbox","iphone");
+
+
+
+
+        for (int i = 0; i < 5; i++) {
+            java.util.List<WebElement> searchResults = driver.findElements(By.xpath("//div[@data-component-type='s-search-result']//h2/a"));
+            WebElement searchResult = searchResults.get(i);
+            searchResult.click();
+
+            // Retrieve the title of the search result
+            WebElement titleElement = driver.findElement(By.id("productTitle"));
+            String title = titleElement.getText();
+            System.out.println("Search Result " + (i + 1) + " Title: " + title);
+
+            driver.navigate().back();
+
+            driver.findElements(By.xpath("//div[@data-component-type='s-search-result']//h2/a"));
+        }
+
+
+
         //çıkan sonuç yazısını konsola yazdırınız
-        //çıkan ürünlerden ilk 5 tanesine tıklayıp sayfa başlıklarını yazdırınız
+        List<WebElement> h2baslik = driver.findElements(By.xpath("//div[@data-component-type='s-search-result']//h2/a"));
+        System.out.println(h2baslik.size());
+
+//        for (WebElement w:h2baslik) {
+//            System.out.println(w.getText());
+//        }
+
+//        for (int i = 0; i < 5; i++) {
+//            WebElement searchResults = h2baslik.get(i);
+//            searchResults.click();
+//            System.out.println(driver.getTitle());
+//            driver.navigate().back();
+//
+//        }
+
+
+
+
+
+        //Utils.sleeping(10000);
+        //driver.close();
     }
 }
